fb7b549935bc95194768b1c8571d792cf4039e69
Dawid Kostka <dawidkostka480@gmail.com>
2026-05-20 21:59:15 +0200

    Initial Commit

---
+++ swiftapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftapp
+//
+//  Created by Dawid Kostka on 20/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftapp/swiftappApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftappApp.swift
+//  swiftapp
+//
+//  Created by Dawid Kostka on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct swiftappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
